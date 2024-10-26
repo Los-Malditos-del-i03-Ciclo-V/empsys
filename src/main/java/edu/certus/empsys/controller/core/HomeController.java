dec7feadaa1e03c26273b640572b9b819ba47d51
Geraldo Jaramillo <Geraldo_07_@outlook.com>
2024-10-26 15:38:29 -0500

    feat(controlador): Se implementa el controlador HomeController
    Se implementa el controlador HomeController con las siguientes funcionalidades:
    
    - `/tabla`: Muestra una tabla de vacantes.
    - `/detalle`: Muestra el detalle de una vacante.
    - `/listado`: Muestra un listado de nombres de empleos.
    - `/`: Muestra la página principal.

---
+++ src/main/java/edu/certus/empsys/controller/core/HomeController.java
@@ -0,0 +1,69 @@
+package edu.certus.empsys.controller.core;
+
+import edu.certus.empsys.entity.vacante.model.Vacante;
+import edu.certus.empsys.service.contract.IVacanteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private IVacanteService vacanteService;
+
+    @GetMapping("/tabla")
+    public String mostrarTabla(Model model) {
+
+        List<Vacante> lista = vacanteService.obtenerTodasLasVacantes();
+
+        model.addAttribute("vacante", lista);
+
+        return "vacantes";
+    }
+
+    @GetMapping("/detalle")
+    public String mostrarDetalle(Model model) {
+
+        Vacante vacante =  Vacante.builder()
+                .nombre("Ingeniero electrónico")
+                .descripcion("Se solicita un Ingeniero electrónico")
+                .fecha(new Date())
+                .salario(4000.00)
+                .build();
+
+        model.addAttribute("vacante", vacante);
+
+        return "detalle";
+    }
+
+    @GetMapping("/listado")
+    public String mostrarListado(Model model) {
+
+        List<String> lista= new LinkedList<String>();
+
+        lista.add("Ingeniería de sistemas");
+        lista.add("Marketing");
+        lista.add("Contador");
+
+        model.addAttribute("empleos", lista);
+
+        return "listado";
+    }
+
+    @GetMapping("/")
+    public String mostrarHome(Model model) {
+
+        List<Vacante> lista = vacanteService.obtenerTodasLasVacantes();
+
+        model.addAttribute("vacante", lista);
+
+        return "home";
+    }
+
+}
