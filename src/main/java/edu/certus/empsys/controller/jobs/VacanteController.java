38c0327b82bc3b5c21354af2460f5a24998d80bb
Geraldo Jaramillo <Geraldo_07_@outlook.com>
2024-10-29 07:47:00 -0500

    feat:(formulario): se implementa validación de formularios para el registro de vacantes
    Se agrega validación al formulario de registro de vacantes para asegurar la integridad de los datos antes de guardarlos en la base de datos.

--- src/main/java/edu/certus/empsys/controller/jobs/VacanteController.java
+++ src/main/java/edu/certus/empsys/controller/jobs/VacanteController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
@@ -39,18 +41,24 @@ public class VacanteController {
     }
 
     @GetMapping("/create")
-    public String crear() {
+    public String crear(Vacante vacante) {
+
 
         return "vacantes/formVacante";
 
     }
 
     @PostMapping("/save")
-    public String guardar(Vacante vacante) {
-
+    public String guardar(Vacante vacante, BindingResult result) {
+        if (result.hasErrors()) {
+            for (ObjectError error : result.getAllErrors()) {
+                System.out.println("Ocurrió un error: " + error.getDefaultMessage());
+            }
+            return "vacantes/formVacante"; // Redirecciona al formulario para mostrar errores
+        }
         vacanteService.guardarVacante(vacante);
-
-        return "redirect:/vacantes/index";
+        System.out.println("Vacante: " + vacante);
+        return "vacantes/listVacantes"; // Redirecciona a la lista de vacantes
     }
 
     @GetMapping("/delete")
