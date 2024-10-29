51d954049c462b97472f4ad6ddc4ab008683f1bd
Geraldo Jaramillo <Geraldo_07_@outlook.com>
2024-10-29 10:53:28 -0500

    feat(categorias): se crea la clase CategoriaFactory
    Se crea la clase `CategoriaFactory` para generar una lista de categorías de forma inicial.
    
    La clase `CategoriaFactory` incluye un método `crearCategorias()` que crea una lista con dos categorías de prueba: "Programador" y "Analista".

---
+++ src/main/java/edu/certus/empsys/entity/categoria/factory/CategoriaFactory.java
@@ -0,0 +1,11 @@
+package edu.certus.empsys.entity.categoria.factory;
+
+import edu.certus.empsys.entity.categoria.model.Categoria;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoriaFactory {
+    public static List<Categoria> listaDeCategorias = new ArrayList<>();
+
+}
